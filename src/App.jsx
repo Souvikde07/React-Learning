4e950e9884e1e2b56d37b11d9ff9d928494057e1
Souvik De <souvik.de@sato-global.com>
2024-06-01 02:51:57 +0900

    add react icons and bit of code to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 
 import { useState } from 'react';
 import './App.css';
+import { RiCheckboxBlankFill } from "react-icons/ri"
+import { RiCheckboxFill } from "react-icons/ri"
+import { RiDeleteBin6Fill } from "react-icons/ri"
+import { TbPencilPlus } from "react-icons/tb"
+
 
 function App() {
 
   return (
     <>
-      
+      <div className='header-container'>
+        <div className='header'>
+          <p className='header-title'>
+            My Tasks
+          </p>
+          <div className='header-add-task'>
+            <p className='header-add-task-text'>
+              <TbPencilPlus/>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
